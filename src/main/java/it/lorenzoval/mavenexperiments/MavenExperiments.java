c049fd8002d7d544a0f4fb3665f14ac5b8e60a7f
Lorenzo Valeriani <lorenzoval98@gmail.com>
2021-03-23 21:17:45 +0100

    Added IOUtils
    Signed-off-by: Lorenzo Valeriani <lorenzoval98@gmail.com>

--- src/main/java/it/lorenzoval/mavenexperiments/MavenExperiments.java
+++ src/main/java/it/lorenzoval/mavenexperiments/MavenExperiments.java
@@ -1,9 +1,24 @@
 package it.lorenzoval.mavenexperiments;
 
+import java.io.InputStream;
+import java.io.IOException;
+
+import java.net.URL;
+import java.net.MalformedURLException;
+
+import org.apache.commons.io.IOUtils;
+
 public class MavenExperiments {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		try (InputStream in = new URL("https://commons.apache.org").openStream()) {
+			System.out.println(IOUtils.toString(in, "UTF-8"));
+		} catch (MalformedURLException mue) {
+			System.out.println("MalformedURLException");
+		} catch (IOException ioe) {
+			System.out.println("IOException");
+		}
+
 	}
 
 }
